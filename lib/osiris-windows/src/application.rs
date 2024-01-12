9ce38df0cc2eacfda62b8698b5157bc036b44c20
David Rheinsberg <david@readahead.eu>
2024-01-12 16:41:35 +0100

    lib/osiris-windows: extend notification support
    Extend the toast-notification support and allow rescinding notifications
    once no longer used.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- lib/osiris-windows/src/application.rs
+++ lib/osiris-windows/src/application.rs
@@ -5,45 +5,104 @@
 //! by several modules to interact with the system representation of the
 //! local application.
 
+use windows;
+
+/// ## Application Setup
+///
+/// The setup structure contains all the parameters required to initialize
+/// the application.
+pub struct Setup<'ctx> {
+    /// Application User Model ID to use, or `None` to inherit it.
+    pub aumid: Option<&'ctx str>,
+}
+
 /// ## Application Context
 ///
 /// The context of the local application, providing access to system APIs
 /// regarding the state and lifetime of the application.
-pub struct Application {
-    pub(crate) id: String,
+pub struct Context {
+    aumid: Option<windows::core::HSTRING>,
 }
 
-impl Application {
-    /// ## Create with an Application ID
+impl<'ctx> Setup<'ctx> {
+    /// ## Initialize the Application
     ///
-    /// Create a new application context with the given application ID. Note
-    /// that application IDs have a well-defined meaning on Windows and must
-    /// be properly acquired through external means.
-    pub fn with_id(id: &str) -> Self {
-        Self {
-            id: id.into(),
-        }
+    /// Perform all application initialization and yield the application
+    /// context ready to be used.
+    pub fn initialize(
+        &self,
+    ) -> Result<Context, Box<dyn std::error::Error>> {
+        // If the caller supplied a AUMID, ensure that we set it on the current
+        // process, so every created entity can inherit it. If none was
+        // provided, query the current value (if any) and cache it.
+        let v_aumid = if let Some(id) = self.aumid {
+            let hstr = windows::core::HSTRING::from(id);
+
+            unsafe {
+                windows::Win32::UI::Shell::SetCurrentProcessExplicitAppUserModelID(
+                    &hstr,
+                )?;
+            }
+
+            Some(hstr)
+        } else {
+            unsafe {
+                windows::Win32::UI::Shell::GetCurrentProcessExplicitAppUserModelID()
+                    .and_then(|v| v.to_hstring())
+                    .ok()
+            }
+        };
+
+        Ok(Context {
+            aumid: v_aumid,
+        })
+    }
+}
+
+impl Context {
+    /// ## Yield AUMID
+    ///
+    /// Yield the AUMID of the application, if any.
+    ///
+    /// The Application User Model ID is used to group runtime entities of
+    /// an application (e.g., windows and processes). An application can use
+    /// multiple ones in a single process to behave as multiple logical
+    /// applications. This field yields the ID that was configured for the
+    /// application root process. If no such ID was configured, `None` is
+    /// returned.
+    pub fn aumid(&self) -> Option<windows::core::HSTRING> {
+        self.aumid.clone()
     }
 
-    /// ## Create Application Context
+    /// ## Yield AUMID or a Fallback Value
     ///
-    /// Create a new application context.
-    pub fn new() -> Self {
-        // XXX: Application IDs are required for many operations. Provide a
-        //      dummy to allow tests to go through.
-        //      This should be dropped once the application model is fully
-        //      figured out.
-        Self::with_id(
-            "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe",
-        )
+    /// Yield the AUMID of the application, but use a fallback value if none
+    /// was set or inherited.
+    ///
+    /// The fallback value represents an application (usually PowerShell) that
+    /// is always available on the system, and thus can be used for
+    /// development purposes.
+    ///
+    /// Use this function with care! Only use it if a fallback value is
+    /// actually desired.
+    pub fn aumid_or_fallback(&self) -> windows::core::HSTRING {
+        self.aumid()
+            .unwrap_or_else(
+                || windows::core::HSTRING::from(
+                    "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe",
+                ),
+            )
     }
 
-    /// ## Perform Application Setup
+    /// ## Claim the Context
     ///
-    /// Perform platform dependent setup operations for the application
-    /// context. This includes operations like establishing COM connections
-    /// or populating caches.
-    pub fn setup(&self) -> Result<(), Box<dyn std::error::Error>> {
-        Ok(())
+    /// A no-op that is used to mark variables as used and prevent them from
+    /// being dropped by refactors.
+    ///
+    /// Application contexts are often implicit by guaranteeing that some
+    /// information of the calling process has been pre-populated. Use this
+    /// function to document that you rely on this behavior.
+    pub fn claim(&self) {
+        // no-op
     }
 }
--- lib/osiris-windows/src/notification.rs
+++ lib/osiris-windows/src/notification.rs
@@ -1,6 +1,6 @@
 //! # User Notification APIs
 //!
-//! This module provides APIs to send notifications to the desktop environment
+//! This module provides APIs to send notifications to the Windows Desktop
 //! to display to the user. These notifications are tied to an application but
 //! are retained by the system even if the application exits.
 //!
@@ -10,14 +10,44 @@
 
 // XXX: The current implementation lacks a few advanced features of the
 //      WinRT Toast API. These can be added in the future. It includes things
-//      like:
-//
-//      - Icons: Specifying icones to be displayed next to a notification.
-//      - Buttons: Action buttons to allow the user to select a non-default
-//                 action.
+//      like buttons and input controls.
+
+// XXX: Text-content starting with `ms-resource:*` references content in the
+//      application resources. We must carefully sanitize user-input to
+//      prevent accidental mis-use.
+//      This is a horrible layer violation by the WinRT API, yet little we
+//      can do about it.
 
 use crate::application;
 use windows;
+
+/// ## Notification Duration
+///
+/// Notifications are shown on screen for a moment. Once they vanish, they are
+/// retained in the notification center. This type allows controlling how long
+/// it stays visible on screen.
+pub enum Duration {
+    Short,
+    Long,
+}
+
+/// ## Notification Scenario
+///
+/// The scenario type controls how a notification is handled by the
+/// notification system, how urgent it is, which default sounds and behaviors
+/// to apply, and how to render it.
+pub enum Scenario {
+    Alarm,
+    IncomingCall,
+    Reminder,
+    Urgent,
+}
+
+impl Default for Scenario {
+    fn default() -> Self {
+        Scenario::Reminder
+    }
+}
 
 /// ## Notification
 ///
@@ -25,7 +55,20 @@ use windows;
 /// will track the lifetime of the notification. The notification object can be
 /// used to raise multiple, possibly independent notifications with similar
 /// content.
+#[derive(Default)]
 pub struct Notification {
+    /// Additional text element used to denote the origin of the notification.
+    pub attribution: Option<String>,
+    /// Time to display, formatted using the ISO 8601 standard.
+    pub display_timestamp: Option<String>,
+    /// Duration to keep the notification on screen for.
+    pub duration: Option<Duration>,
+    /// Header information to group notifications.
+    pub header: Option<(String, String)>,
+    /// Scenario information for the notification.
+    pub scenario: Scenario,
+    /// Text elements of the notification.
+    pub text: Vec<String>,
 }
 
 /// ## Notification Handle
@@ -40,7 +83,9 @@ pub struct Notification {
 /// A notification handle is tied to an application context and cannot outlive
 /// it. However, a notification handle can be recreated from an application
 /// context and a notification ID.
-pub struct Handle {
+pub struct Handle<'ctx> {
+    app: &'ctx application::Context,
+    id: (Option<String>, Option<String>),
 }
 
 impl Notification {
@@ -49,15 +94,131 @@ impl Notification {
     /// Create a pristine notification object ready to be populated with
     /// data.
     pub fn new() -> Self {
-        Self {
+        Default::default()
+    }
+
+    fn build_xml(
+        &self,
+    ) -> Result<
+        windows::Data::Xml::Dom::XmlDocument,
+        Box<dyn std::error::Error>
+    > {
+        // Build the Notification XML
+        //
+        // The content of a notification (and some metadata) is all encoded in
+        // an XML document specified by Microsoft [^xml]. It should be rather
+        // straightforward to follow.
+        //
+        // [^xml]: https://learn.microsoft.com/en-us/uwp/schemas/tiles/toastschema/root-elements
+        let xml = windows::Data::Xml::Dom::XmlDocument::new()?;
+
+        // Create top-level <toast>
+        //
+        // This is the root element and defines basic notification properties
+        // like duration and scenario.
+        let v_toast = xml.CreateElement(windows::core::h!("toast"))?;
+        if let Some(ref v) = self.duration {
+            v_toast.SetAttribute(
+                windows::core::h!("duration"),
+                &match v {
+                    Duration::Short => windows::core::HSTRING::from("short"),
+                    Duration::Long => windows::core::HSTRING::from("long"),
+                },
+            )?;
         }
+        if let Some(ref v) = self.display_timestamp {
+            v_toast.SetAttribute(
+                windows::core::h!("displayTimeStamp"),
+                &windows::core::HSTRING::from(v),
+            )?;
+        }
+        v_toast.SetAttribute(
+            windows::core::h!("scenario"),
+            &match self.scenario {
+                Scenario::Alarm => windows::core::HSTRING::from("alarm"),
+                Scenario::IncomingCall => windows::core::HSTRING::from("incomingCall"),
+                Scenario::Reminder => windows::core::HSTRING::from("reminder"),
+                Scenario::Urgent => windows::core::HSTRING::from("urgent"),
+            },
+        )?;
+
+        // Create <header>
+        //
+        // A header allows grouping notifications. It has an ID to identify
+        // it, as well as a title that is shown by the notification system.
+        if let Some((ref id, ref title)) = self.header {
+            let v_header = xml.CreateElement(windows::core::h!("header"))?;
+            v_header.SetAttribute(
+                windows::core::h!("id"),
+                &windows::core::HSTRING::from(id),
+            )?;
+            v_header.SetAttribute(
+                windows::core::h!("title"),
+                &windows::core::HSTRING::from(title),
+            )?;
+            v_toast.AppendChild(&v_header)?;
+        }
+
+        // Create <visual>
+        //
+        // This contains all visual elements that are not auto-generated. That
+        // is, this contains text and image elements to show in the
+        // notification.
+        let v_visual = xml.CreateElement(windows::core::h!("visual"))?;
+
+        // Create <binding>
+        //
+        // This element used to bind to a particular template, yet nowadays is
+        // used to build up the generic template. It is effectively a wrapper
+        // around the visual content.
+        let v_binding = xml.CreateElement(windows::core::h!("binding"))?;
+        v_binding.SetAttribute(
+            windows::core::h!("template"),
+            windows::core::h!("ToastGeneric"),
+        )?;
+
+        // Append <text> Nodes
+        //
+        // Up to 3 text-nodes are supported, each being rendered as a an
+        // individual line. An additional text-node can be used to render the
+        // source information of the notification (called `attribution`).
+        //
+        // We allow adding any number of text-nodes, but these will likely be
+        // ignored by the notification server.
+        let mut n_text = 0;
+        for v in &self.text {
+            let v_text = xml.CreateElement(windows::core::h!("text"))?;
+            n_text += 1;
+            v_text.SetAttribute(
+                windows::core::h!("id"),
+                &windows::core::HSTRING::from(format!("{}", n_text)),
+            )?;
+            v_text.SetInnerText(&windows::core::HSTRING::from(v))?;
+            v_binding.AppendChild(&v_text)?;
+        }
+        if let Some(ref v) = self.attribution {
+            let v_text = xml.CreateElement(windows::core::h!("text"))?;
+            n_text += 1;
+            v_text.SetAttribute(
+                windows::core::h!("id"),
+                &windows::core::HSTRING::from(format!("{}", n_text)),
+            )?;
+            v_text.SetInnerText(&windows::core::HSTRING::from(v))?;
+            v_binding.AppendChild(&v_text)?;
+        }
+
+        v_visual.AppendChild(&v_binding)?;
+        v_toast.AppendChild(&v_visual)?;
+        xml.AppendChild(&v_toast)?;
+
+        Ok(xml)
     }
 
     /// ## Raise Notification
     ///
     /// Raise the notification and send them to the notification server. This
-    /// will use the specified application context to access the required
-    /// system APIs.
+    /// will operate on behalf of the running process and its application ID.
+    /// A suitable application context must be provided.
     ///
     /// If a notification identifier is specified, any previous notification
     /// of the same identifier is replaced and a handle for the notification
@@ -66,48 +227,98 @@ impl Notification {
     /// If no notification identifier is specified, the notification is still
     /// raised, but no handle is returned, nor is any other notification
     /// replaced.
+    ///
+    /// The notification identifier is a pair of group and tag. Both must match
+    /// for the identifier to be considered identical. You are free to use only
+    /// one of them. They used to be limited to 16 characters, which was later
+    /// extended to 64 characters.
     pub fn raise<'ctx>(
         &self,
-        app: &'ctx application::Application,
-        id: Option<String>,
-    ) -> Option<()> {
-        let xml = windows::Data::Xml::Dom::XmlDocument::new().unwrap();
+        app: &'ctx application::Context,
+        id: (Option<String>, Option<String>),
+    ) -> Result<Handle<'ctx>, Box<dyn std::error::Error>> {
+        // `GetDefault()` queries the current user and `CreateToastNotifier()`
+        // queries the AUMID of the application. Hence, claim the application
+        // context to have something where we could reasonbly tie that
+        // information to.
+        app.claim();
 
-        xml.LoadXml(
-            &windows::core::HSTRING::from(
-                r#"
-                <toast
-                    duration="long"
-                    scenario="reminder"
-                >
-                    <visual>
-                        <binding
-                            template="ToastGeneric"
-                        >
-                            <text id="1">Title</text>
-                        </binding>
-                    </visual>
-                </toast>"#,
-            ),
-        ).unwrap();
+        let mgr = windows::UI::Notifications::ToastNotificationManager::GetDefault()?;
+        let notifier = mgr.CreateToastNotifier()?;
+        let xml = self.build_xml()?;
 
-        let notification = windows::UI::Notifications::ToastNotification::CreateToastNotification(
-            &xml,
-        ).unwrap();
-
-        let toast_notifier = windows::UI::Notifications::ToastNotificationManager::CreateToastNotifierWithId(
-            &windows::core::HSTRING::from(&app.id),
-        ).unwrap();
-
-        toast_notifier.Show(&notification).unwrap();
-
-        std::thread::sleep(std::time::Duration::from_millis(10));
-
-        if let Some(_id_str) = id {
-            Some(())
-        } else {
-            None
+        let notification = windows::UI::Notifications::ToastNotification::CreateToastNotification(&xml)?;
+        if let Some(ref v) = id.0 {
+            notification.SetGroup(&windows::core::HSTRING::from(v))?;
         }
+        if let Some(ref v) = id.1 {
+            notification.SetTag(&windows::core::HSTRING::from(v))?;
+        }
+
+        notifier.Show(&notification)?;
+
+        Ok(Handle::with_id(app, id))
+    }
+}
+
+impl<'ctx> Handle<'ctx> {
+    /// ## Create Notification Handle from its ID
+    ///
+    /// Re-create a notification handle from its ID. This handle will be
+    /// functionally identical to the original handle. The application
+    /// context does not have to be the same as was used to raise the
+    /// notification. However, it must have a matching application ID set.
+    pub fn with_id(
+        app: &'ctx application::Context,
+        id: (Option<String>, Option<String>),
+    ) -> Self {
+        Self {
+            app: app,
+            id: id,
+        }
+    }
+
+    /// ## Rescind a Notification
+    ///
+    /// Rescind a notification that was previously raised. This will remove
+    /// the notification from the notification server and consume the handle.
+    pub fn rescind(
+        self,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        // If no ID was used, it was a one-shot notification that cannot be
+        // rescinded (or is automatically rescinded). Nothing to do.
+        if self.id.0.is_none() && self.id.1.is_none() {
+            return Ok(());
+        }
+
+        // `GetDefault()` queries the current user and `CreateToastNotifier()`
+        // queries the AUMID of the application. Hence, claim the application
+        // context to have something where we could reasonbly tie that
+        // information to.
+        self.app.claim();
+
+        let mgr = windows::UI::Notifications::ToastNotificationManager::GetDefault()?;
+        let notifier = mgr.CreateToastNotifier()?;
+
+        // Create a simple template notification to avoid complex and
+        // unnecessary XML operations.
+        let xml = windows::UI::Notifications::ToastNotificationManager::GetTemplateContent(
+            windows::UI::Notifications::ToastTemplateType::ToastText01,
+        )?;
+        let notification = windows::UI::Notifications::ToastNotification::CreateToastNotification(&xml)?;
+
+        // Set the same IDs as the original notification.
+        if let Some(ref v) = self.id.0 {
+            notification.SetGroup(&windows::core::HSTRING::from(v))?;
+        }
+        if let Some(ref v) = self.id.1 {
+            notification.SetTag(&windows::core::HSTRING::from(v))?;
+        }
+
+        // Remove the notification.
+        notifier.Hide(&notification)?;
+
+        Ok(())
     }
 }
 
@@ -117,9 +328,12 @@ mod tests {
 
     #[test]
     fn show() {
-        let app = application::Application::new();
+        let app = application::Setup {
+            aumid: None,
+        }.initialize().unwrap();
+
         let notify = Notification::new();
 
-        notify.raise(&app, None);
+        notify.raise(&app, (None, None)).unwrap();
     }
 }
